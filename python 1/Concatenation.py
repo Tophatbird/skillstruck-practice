7893f66c5fed3475b019ab3ed4d143b6a7d394c5
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:01 -0700

    exported from Skill Struck

---
+++ python 1/Concatenation.py
@@ -0,0 +1,12 @@
+#seperate the variables
+string1 = "help"
+string2 =  "me"
+string3 =  "please"
+
+together =  string1 + " " + string2 + " " + string3
+# the plus combines the variables.
+# add spaces in the together variable by adding a space with around it
+print(together)
+
+#add stuff at the start and end by adding qoutes around the phrase before the varible
+# example: print("dont " + (together) + " thank you.")

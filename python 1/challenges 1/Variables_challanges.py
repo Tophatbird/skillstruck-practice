8ea6d4454a2b20a7a00e9d0519680d2a64b5d456
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:19 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Variables_challanges.py
@@ -0,0 +1,18 @@
+#Riddle Variables
+what_goes_up_when_rain_comes_down = "the sun"
+
+what_is_full_of_holes_but_holds_water = "sponge"
+
+what_can_you_not_keep_until_you_give_it = "birth"
+
+print(what_goes_up_when_rain_comes_down)
+print(what_is_full_of_holes_but_holds_water)
+print(what_can_you_not_keep_until_you_give_it)
+
+#How many in a beetle?
+horse = 1
+hamburger =250
+bodies = 6.5
+print(horse)
+print(hamburger)
+print(bodies)

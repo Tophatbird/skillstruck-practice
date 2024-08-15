b661fd7ea0dec309ba1273f20f8bb297ef2df0e2
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:24 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Converting_ challenges.py
@@ -0,0 +1,14 @@
+#Name Repeater
+name = input('What is your name?')
+age = int(input(' what is your age'))
+
+dupe = name * age
+
+print(dupe)
+
+#Decimal Dilemma
+
+number = float(input('enter a number with a decimal'))
+
+whole = int(number)
+print(whole)

95ed4e7f29843ee978183ec551a606c182a404e4
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:18 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Intro_Challenges.py
@@ -0,0 +1,10 @@
+#First message
+print("I love to code!")
+
+
+#Your Favorite Things to Do.
+print("I like to draw")
+print("I like to sleep")
+print("I like to game")
+print("I like to code")
+print("I like to murder")

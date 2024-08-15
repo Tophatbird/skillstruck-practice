3e5d40531b24a43cc0834a8bb9efd0605339b7f4
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:21 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Input_Output_Challenges.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+experience = input("What is your music experience?")
+
+print("Hello " + name + " welcome to the guitar class! Your music experience: " + experience)
+
+dessert = input("What is your favorite dessert?")
+print(dessert)
+
+compliment = input("state a compliment")

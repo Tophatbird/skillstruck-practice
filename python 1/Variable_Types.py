9d6e655a2cc2e88406ee0979a63eeafdbe58ab9f
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:22 -0700

    exported from Skill Struck

---
+++ python 1/Variable_Types.py
@@ -0,0 +1,12 @@
+string = "word"
+#Needs qoutes around it
+integer = 4
+#dosent need qoutes
+boolean = True
+#booleans must be capatilized like True/False
+
+print(string)
+
+print(integer)
+
+print(boolean)

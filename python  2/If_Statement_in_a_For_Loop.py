8c72227e99c3bef7d24d4e486f98ef1cad4bd973
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:09 -0700

    exported from Skill Struck

---
+++ python  2/If_Statement_in_a_For_Loop.py
@@ -0,0 +1,14 @@
+foods = ["mushrooms", "broccoli", "fish"]
+
+for x in foods:
+    if x == "mushrooms":
+        print( x + " are gross.")
+    elif x == "broccoli": 
+        print( x + " is gross.")
+    elif x == "fish" :
+        print( x + " is gross.")
+    else: 
+        print("No food is gross")
+
+
+        

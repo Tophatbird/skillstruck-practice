500a9c8ad22d405ef30f55f6516cb9f6ca2588c5
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:23 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Variable_Types_Challenges.py
@@ -0,0 +1,24 @@
+# My Weekend
+did_do = "stayed up"
+wish_you_did = "get sleep"
+
+meals = 7
+homework = 0
+
+tiered = True
+happy = False
+
+# About me quiz
+
+is_scared_of_heights =False
+loves_dogs = True
+has_brown_eyes = True
+knows_html = False
+ate_breakfast_today = True
+
+
+print(is_scared_of_heights)
+print(loves_dogs)
+print(has_brown_eyes)
+print(knows_html)
+print(ate_breakfast_today)

efdf412c59ee9b1d0d04d1a9f7c8ab417f89582f
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:24 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Numbers_Challenges.py
@@ -0,0 +1,20 @@
+# #Specific Answers
+I_enjoy_working_in_teams = 1
+I_enjoy_working_in_teams = 1.2
+
+I_like_to_try_new_things = 2
+I_like_to_try_new_things = 2.3
+
+I_love_to_be_outside = 2
+I_love_to_be_outside = 2.2
+
+I_want_to_travel_the_world = 1
+I_want_to_travel_the_world = 1.6
+
+School_can_be_way_hard_for_me = 4
+School_can_be_way_hard_for_me = 4.5
+
+#How many pets?
+
+average_pets = 6.5
+print(average_pets)

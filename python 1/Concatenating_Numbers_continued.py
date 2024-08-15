43ec3b347382a89399e780b900ed5281689aef0b
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:05 -0700

    exported from Skill Struck

---
+++ python 1/Concatenating_Numbers_continued.py
@@ -0,0 +1,17 @@
+string1 = "you can have multiple numbers like {} and {}"
+value1 = 1
+value2 = 3
+print(string1.format(value1, value2))
+#this is just the last lessons note btw
+
+
+#something new is that if you put numbers in the curly brackets (starting from 0) you can choose what goes where
+#string1 = "you can have multiple numbers {0} , {3} , {2} , {1} and {4}
+#value1 = 1
+#value2 = 2
+#value3 = 3
+#value4 = 4
+#value5 = 5
+
+#print(string1.format(value1, value2, value3, value4, value5))
+#this will go in order of 0-4 but the print will put the numbers in the order you listed 

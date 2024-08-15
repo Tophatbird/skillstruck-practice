b75429f97069320752685949fccbb9ea2a937eb7
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:22 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Syntax_and_Comments_challenges.py
@@ -0,0 +1,17 @@
+#Snow Day!
+morning = input("What would you do in the morning?")
+afternoon = input("What would you do in the morning?")
+evening = input("What would you do in the morning?")
+
+print("you would " + morning + " in the morning")
+print("you would " + afternoon + " in the afternoon")
+print("you would " + evening + " in the evening")
+
+#Your Favorite Movies
+
+print("my third favorite movie is uhhh")
+#these are
+print("my second favorite movie is billy boy")
+#not
+print("my favorite movie is dead guy 6")
+#real movies

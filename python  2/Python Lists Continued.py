8e3221b1100942b7f02def5e6be4ff2d6d5d35c1
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:11 -0700

    exported from Skill Struck

---
+++ python  2/Python Lists Continued.py
@@ -0,0 +1,14 @@
+list = ["car", "box", "flesh", "Joe", "Hamburger", "child", "rat"]
+
+print(list[0:3])
+ 
+list[6] = "bomb"
+
+print(list)
+
+print(len(list))
+
+flowers = ["rose", "tulip", "daffodil", "dandelion", "sunflower", "lily", "lotus"]
+
+flowers.sort(reverse)
+print(flowers)

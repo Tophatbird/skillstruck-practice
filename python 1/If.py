8d3857fd821b5a472bde785a44179056d07e0d30
Tophatbird <davidavilajr2008@gmail.com>
2024-08-15 09:32:07 -0700

    exported from Skill Struck

---
+++ python 1/If.py
@@ -0,0 +1,14 @@
+varible1 = 8
+varible2 = 7
+if varible1 > varible2:
+    print("8 is a bigger number")
+
+
+
+
+varible3 = 3
+varible4 = 9
+if varible3 > varible4:
+    print("3 is a bigger number")
+else:
+    print("9 is a bigger number")

d7d4f37e8b6d67092f845c7d2d8f0eb45d62cec6
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:00 -0700

    exported from Skill Struck

---
+++ python 1/practice/Concatenating_Numbers.py
@@ -0,0 +1,8 @@
+string1 = "the number of mice living in my walls is {}"
+mice = 90
+print(string1.format(mice))
+#you can also have multiple integers inside the format method like this
+#string1 = "you can have multiple numbers like {} and {}
+#value1 = 1
+#value2 = 3
+#print(string1.format(value1, value2))

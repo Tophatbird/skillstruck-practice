8bd0f580be2723226431492c45849d91608edaa4
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:13:56 -0700

    exported from Skill Struck

---
+++ python 1/practice/modulus.py
@@ -0,0 +1,8 @@
+user_number = int(input("choose a number to divide by 7 "))
+number = user_number % 5
+print(number)
+# modulus ONLY prints the remainder
+
+#Test for even or odd using modulus:
+#If number % 2 == 0 then number is odd
+#If number % 2 == 1 then number is even

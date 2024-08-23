6b5f913ea55c5c0427fed168024c27f9dfbce7d8
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:15 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/String Methods Challenges.py
@@ -0,0 +1,12 @@
+# Group Thank You Message
+message = input('write a thank you message with several instances of the word "I" in it.')
+
+
+
+print(message.replace("I", "We"))
+
+#Where is the @?
+
+no_at = input('write a sentence with several of the "@" in it.')
+
+print(no_at.replace("@", ""))

94b611a580760b2d48098d31b7affb39517f7b31
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:03 -0700

    exported from Skill Struck

---
+++ python 1/practice/Python_Conditionals.py
@@ -0,0 +1,9 @@
+age = int(input("What is your age"))
+print(age)
+license = False
+if age >= 16 and license == True:
+    print("You are old enough to drive.")
+else: 
+    print("You are not able to drive.")
+
+# value != value : this checks if the conditon is not equal

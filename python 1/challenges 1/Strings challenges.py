77171d49d145dd8e2e3f00b910357d8d16c44fd3
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:14 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Strings challenges.py
@@ -0,0 +1,35 @@
+#Best Names
+name1 = "Toaster"
+
+name2 = "Microwave"
+
+name3 = "Fridge"
+
+name4 = "Blender"
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+
+# Garden and Orchard
+
+fruit1 = "Durian"
+
+fruit2 = "Passion Fruit"
+
+fruit3 = "Avacodo"
+
+veggie1 = "Cabage"
+
+veggie2 = "Let us"
+
+veggie3 = "The void"
+
+
+print(fruit1)
+print(fruit2)
+print(fruit3)
+print(veggie1)
+print(veggie2)
+print(veggie3)

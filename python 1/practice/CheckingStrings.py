601396a50c3fcc765ee2ed472fdc6c2b574eaa69
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:13:59 -0700

    exported from Skill Struck

---
+++ python 1/practice/CheckingStrings.py
@@ -0,0 +1,9 @@
+string1 = "this is a test"
+check = "is" in string1
+print(check)
+#this checks if the word is in the statement
+
+string1 = "this is a test"
+check = "is" not in string1
+print(check)
+#this checks if the word is not in the statement

c31a8d7c85154beee913a2e6050bd4ad26b84394
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:13:57 -0700

    exported from Skill Struck

---
+++ python 1/practice/StringMethods.py
@@ -0,0 +1,11 @@
+string1 = "beesechurger"
+print(string1.upper())
+#makes all uppercase
+
+string2 = "DoThOg"
+print(string2.lower())
+#makes all lowercase
+
+string3 = "flobber glumbus yurbert vroge"
+print(string3.replace("glumbus", "smumber"))
+#replaces a word

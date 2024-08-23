1b2fb6357bb7e996fd41c7d3a15ebc802ad0fd27
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:19 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/Python If Statements challenges.py
@@ -0,0 +1,14 @@
+# Odd or Even
+number = int(input("enter a number"))
+if number % 2 == 0:
+    print("Even")
+else:
+   print("Odd")
+
+# Divisible by 3?
+
+number = 64
+if number % 3 == 0:
+    print("divisble by 3")
+else:
+    print("not divisble by 3")

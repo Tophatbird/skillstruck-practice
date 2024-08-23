7910e72c20c14f3224a52dc8ecc375da2106ee10
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:14:19 -0700

    exported from Skill Struck

---
+++ python 1/challenges 1/lists challenges.py
@@ -0,0 +1,18 @@
+places = ["home", "walmart", "Earth", "The moon", "The underorld"]
+print(places)
+
+
+
+
+#1. How many slices of pizza did you eat?
+
+#2. How many new places did you go?
+
+# 3. Name one friend you hung out with.
+
+# 4. Name one news event that happened this year.
+
+# 5. How many teachers have you had?
+
+memories = [956576, 3, "none", "apocolips", 6]
+print(memories)

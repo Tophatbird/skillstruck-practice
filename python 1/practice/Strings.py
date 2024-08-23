ffc86cdf98b3f53b75c758e25d3d6f76442825c8
Tophatbird <davidavilajr2008@gmail.com>
2024-08-23 08:13:57 -0700

    exported from Skill Struck

---
+++ python 1/practice/Strings.py
@@ -0,0 +1,6 @@
+multiline = """ruih8rieuiriurb ihrheabivbaibfhabjksbjkbvruiebriubvuibwvuiebeuiiuebuiewbueu
+iiubviuviuiuebuivebbsbvebuebuibeuibvubvbuivbe
+jkvuideuibivbiebiubvbudbdeueb
+u44uthui"""
+print(multiline)
+# multilines require three qoute marks around them like this: '''this'''

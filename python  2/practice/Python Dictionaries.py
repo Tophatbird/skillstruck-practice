74c7e1b49680e52938ed884ed7109054f6132063
Tophatbird <davidavilajr2008@gmail.com>
2024-08-30 07:47:43 -0700

    exported from Skill Struck

---
+++ python  2/practice/Python Dictionaries.py
@@ -0,0 +1,14 @@
+mountains = {
+    "Timpanogos" : 4,
+    "Everest" : 7,
+    "kilimanjaro" : 6,
+    "Vesuvius" : 3,
+}
+
+print(mountains)
+
+print(mountains["Vesuvius"])
+
+mountains["Kilimanjaro"] = 8
+
+print(mountains)

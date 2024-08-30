b40a51260bf11cc745b4cbaefb134f573548b290
Tophatbird <davidavilajr2008@gmail.com>
2024-08-30 07:47:45 -0700

    exported from Skill Struck

---
+++ python  2/practice/Looping through a Dictionary Continued.py
@@ -0,0 +1,14 @@
+ride = {
+    "joe" :1,
+    "bob" :100,
+    "kyle" :80,
+    "john" :70,
+    "geoff" :900,
+}
+for x in ride.values():
+    print(x)
+
+if x >= 100:
+        print("this person is tall enough to ride..")
+else:
+            print("this person is too short to ride.")

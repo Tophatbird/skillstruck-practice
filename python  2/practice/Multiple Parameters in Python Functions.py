7f21c729336f919e594b194564f2ce0c4abed916
Tophatbird <davidavilajr2008@gmail.com>
2024-08-30 07:47:43 -0700

    exported from Skill Struck

---
+++ python  2/practice/Multiple Parameters in Python Functions.py
@@ -0,0 +1,6 @@
+def pet(first, second) :
+    print("I have a dog, his fur color is " + first)
+    print("I also have a cat, his fur is " + second)
+
+pet("brown!", "black!")
+pet("white!", "orange!")

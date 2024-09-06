815a1f5a53883511c0e3a33e54cb5ac60fa831f8
Tophatbird <davidavilajr2008@gmail.com>
2024-09-06 08:09:18 -0700

    exported from Skill Struck

--- python  2/challenges/Using Integers from a List Challenges.py
+++ python  2/challenges/Using Integers from a List Challenges.py
@@ -1,13 +1,15 @@
 # Multiply the List
 my_list = [int(n) for n in input().split()]
 
+product = 1
+for number in my_list :
+    product = product * number
+print(product)
 
+# Add the List
+my_list = [int(n) for n in input().split()]
 
-
-
-
-
-# product = 
-# for x in :
-#   	product = 
-# print()
+product = 0
+for number in my_list :
+    product = product + number
+print(product)

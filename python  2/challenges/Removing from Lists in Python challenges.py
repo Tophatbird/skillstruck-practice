3620952144dba405a5ede970855095eb221cebac
Tophatbird <davidavilajr2008@gmail.com>
2024-09-06 08:09:20 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Removing from Lists in Python challenges.py
@@ -0,0 +1,13 @@
+# beans
+# my_list = input("enter a list of beans").split()
+
+# if "lima" in my_list :
+# 	my_list.remove("lima")
+# print(my_list)
+# Skills
+
+my_list = input("enter a list of names").split()
+
+medal = my_list.pop(2)
+
+print(medal + " got the bronze medal")

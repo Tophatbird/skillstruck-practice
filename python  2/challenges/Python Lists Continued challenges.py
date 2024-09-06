d664a3dbe89659fc6360f9b4992c68ac4d6f007c
Tophatbird <davidavilajr2008@gmail.com>
2024-09-06 08:09:20 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Python Lists Continued challenges.py
@@ -0,0 +1,10 @@
+# Find the largest value
+
+# my_list = [int(n) for n in input().split()]
+# biggest_number = 0
+# for this_number in my_list:
+# 	if this_number > biggest_number :
+# 		biggest_number = this_number
+
+# print(biggest_number)
+

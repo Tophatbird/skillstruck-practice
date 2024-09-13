1f48b0058924e13ee27e38e5202771784dfd8447
Tophatbird <davidavilajr2008@gmail.com>
2024-09-13 07:44:03 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Python While Loops Challenges.py
@@ -0,0 +1,16 @@
+#How many numbers?
+
+number = int(input()) #asks the user for an input
+counter = 0 #the counter will go up for every number inputted thats not 0
+while number > 0 : # this will make it go until 0 is input
+    number = int(input()) #this is to repeat the input prompt
+    counter += 1 #adds to the counter
+print(counter) #prints the awnser
+
+#Sum of numbers
+number = int(input()) #asks the user for an input
+total = 0 + number #
+while number > 0 : # this will make it go until 0 is input
+    number = int(input()) #this is to repeat the input prompt
+    total += number 
+print(total) #prints the sum

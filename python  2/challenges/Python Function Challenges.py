ff815cedb9ce4805a1292707d82952e7e1313b32
Tophatbird <davidavilajr2008@gmail.com>
2024-09-13 07:44:04 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Python Function Challenges.py
@@ -0,0 +1,15 @@
+# Charlie the Dog
+def charlie():
+    print("Charlie is the god of this realm")
+charlie()
+
+# Start the Game
+start = input("Would you like to launch the game? enter yes or no")
+
+def start_game():
+    if start == "yes" :
+        print("Initialization Complete")
+    else:
+        print("Initialization Failed")
+
+start_game()

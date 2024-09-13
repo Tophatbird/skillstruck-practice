b3550e1015f74c0104c21173d7ad1a5f6d0462d1
Tophatbird <davidavilajr2008@gmail.com>
2024-09-13 07:44:02 -0700

    exported from Skill Struck

--- python  2/challenges/Python Lists Continued challenges.py
+++ python  2/challenges/Python Lists Continued challenges.py
@@ -1,10 +1,21 @@
 # Find the largest value
 
-# my_list = [int(n) for n in input().split()]
-# biggest_number = 0
-# for this_number in my_list:
-# 	if this_number > biggest_number :
-# 		biggest_number = this_number
+my_list = [int(n) for n in input().split()]
+biggest_number = 0
+for this_number in my_list:
+	if this_number > biggest_number :
+		biggest_number = this_number
 
-# print(biggest_number)
+print(biggest_number)
 
+
+# Greater than left index
+
+my_list = [int(n) for n in input().split()]
+
+current = my_list[0]
+
+for number in my_list :
+	if number > current :
+		print(number)
+	current = number

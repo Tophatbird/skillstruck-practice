9a8b92610b6a8444ecee0e719c85d9ee73a45179
Tophatbird <davidavilajr2008@gmail.com>
2024-10-22 09:41:09 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Multiple Parameters in Python Functions Challenges.py
@@ -0,0 +1,23 @@
+# # Hungry For Apples 3
+# fruit1 = input("Please enter the name of a fruit: ")
+# fruit2 = input("Please enter the name of a fruit: ")
+# fruit3 = input("Please enter the name of a fruit: ")
+
+# def hungryForApples(fruit1,fruit2,fruit3):
+#     print(fruit1+fruit2+fruit3)
+	
+# hungryForApples(fruit1,fruit2,fruit3)
+
+
+# Find the Smallest Integer Function
+choice1 = int(input("What is the first number?"))
+choice2 = int(input("What is the second number?"))
+
+def my_function(first, second):
+	if  first < second :
+		print(first)
+	else:
+		print(second)
+		
+
+my_function(choice1,choice2)

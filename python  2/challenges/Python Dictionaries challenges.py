b1e12a29aaeba0f6f1ebf46bd7fa6223b1377a05
Tophatbird <davidavilajr2008@gmail.com>
2024-10-22 09:41:10 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Python Dictionaries challenges.py
@@ -0,0 +1,17 @@
+# Survival
+
+# knife = int(input("From 1-10, how important is a knife?"))
+# fire_starter = int(input("From 1-10, how important is a fire starter?"))
+# pot = int(input("From 1-10, how important is a pot?"))
+# rope = int(input("From 1-10, how important is a rope?"))
+# tarp = int(input("From 1-10, how important is a tarp?"))
+
+# survive = { "knife" : knife , "fire starter": fire_starter , "pot": pot , "rope": rope , "tarp": tarp }
+
+# print(survive)
+
+# Dictionary Author
+
+Dictionary = { "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom", "your" : "mom" }
+
+print(Dictionary)

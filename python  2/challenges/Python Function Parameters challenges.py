ebb7bc8edb33b618ace4f77d2d4e4f0c9a7baf0b
Tophatbird <davidavilajr2008@gmail.com>
2024-10-22 09:41:08 -0700

    exported from Skill Struck

---
+++ python  2/challenges/Python Function Parameters challenges.py
@@ -0,0 +1,33 @@
+# Hungry For Apples 2
+# fruit = input("Please enter the name of a fruit: ")
+
+# def hungryForApples(choice):
+#     print(choice + choice + choice)
+	
+# hungryForApples(fruit)
+
+# Odd/Even Checking Function
+
+def odd_even(num):
+    if num % 2 == 0 :
+        print("even")
+    else:
+        print("odd")
+
+odd_even(1)
+odd_even(2)
+odd_even(3)
+
+# this checks for your number instead
+#userNUMBER = input("enter a number")
+# def odd_even(num):
+#     num = int(num)
+#     if num % 2 == 1 :
+# 		print("odd")
+# 	else:
+# 		print("even")
+
+# odd_even(1)
+# odd_even(2)
+# odd_even(3)
+# odd_even(userNUMBER)
